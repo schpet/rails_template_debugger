c554538d93cbc7af47841502553026645a1d1302
Peter Schilling <peter@peterschilling.org>
2018-08-18 10:19:04 -0700

    Copy over the debugging hack

--- lib/rails_template_debugger.rb
+++ lib/rails_template_debugger.rb
@@ -1,5 +1,31 @@
 require "rails_template_debugger/version"
 
 module RailsTemplateDebugger
-  # Your code goes here...
+  def self.debug_templates
+    ActionView::Template.send(:prepend, ActionViewTemplateRenderWithComment)
+  end
+
+  module ActionViewTemplateRenderWithComment
+    def render(view, locals, buffer = nil, &block)
+      return super unless html_template?
+
+      path = identifier.remove(Rails.root.to_s + "/")
+      # path_short = path.match(/app\/views\/(.*)/)&.captures&.first
+
+      [
+        "<!-- START #{path} -->",
+        "<meta name='debug-template-start' path='#{path}' />",
+        super,
+        "<!-- END #{path} -->"
+      ].join("\n").html_safe
+    end
+
+    private
+
+    def html_template?
+      identifier =~ /\.html(?:\.\w+)?$/ && !(identifier =~ /\/layouts\//)
+    end
+  end
 end
+
+
